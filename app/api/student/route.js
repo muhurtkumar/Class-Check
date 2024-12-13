757cc2eeb8ef790ff9084f66e85ba05c7feadb48
muhurtkumar <muhurtkumar@gmail.com>
2024-12-13 08:16:39 +0530

    Create route.js

---
+++ app/api/student/route.js
@@ -0,0 +1,32 @@
+import { STUDENTS } from "@/utils/schema";
+import { NextResponse } from "next/server";
+import { db } from "@/utils";
+import { eq } from "drizzle-orm";
+
+export async function POST(req,res){
+    const data=await req.json();
+
+    const result=await db.insert(STUDENTS)
+    .values({
+        name:data?.name,
+        grade:data?.grade,
+        address:data?.address,
+        contact:data?.contact
+    })
+
+    return NextResponse.json(result);
+}
+
+export async function GET(req){
+    const result = await db.select().from(STUDENTS);
+    return NextResponse.json(result);
+}
+
+export async function DELETE(req){
+    const searchParams=req.nextUrl.searchParams;
+    const id=searchParams.get('id');
+
+    const result = await db.delete(STUDENTS).where(eq(STUDENTS.id,id));
+
+    return NextResponse.json(result);
+}
